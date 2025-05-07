ae78cd13e16abd1f6fc0d9fd4cec906bf1b45570
Eromose1234567 <alieromose@gmail.com>
2025-05-07 08:03:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+//primisive datatype
+
+let name ="ali" //string
+ //alert(name) //outputing method
+
+ const bbb = "oluwaseyifunmi"
+ console.log(bbb)
+
+ var dfg ="zeetech "
+ document.write(dfg)
+
+
+ const no ="09087656645" //number
+ //alert(no)
+
+ //let isHeAboy =  true;
+ document.write(isHeAboy) //boolean
+
+ //non prrimisive datatype
+ //object
+ let person = {
+    firstname: "ali",
+    lastname: "oluwale",
+    age: "20",
+    occupation: "wed developer",
+    maritalStatus: "single",
+    address: "23, idi iroko close"
+ }
+
+ document.write(`my name is${person.firstname} ${person.lastname}`)
